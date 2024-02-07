cf422c85a7cee3d2856f64b5f3a9fb01ae988aef
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-02-06 22:55:39 -0500

    Add image

--- index.php
+++ index.php
@@ -3,5 +3,16 @@ $title = 'Home';
 include('shared/header.php');
 ?>
 <h1> Electric Vehicles in the 2024 Market </h1>
+<p> 
+    What is your favorite EV in 2024?
+    
+</p>
+<p>
+    <img src="image/COMP1006-As1.png" alt="Homepage Image" width=300 height=300>
+</p>
+<p> 
+    Check out our EV list <a href="vehicle-list.php"> here</a>.
+</p>
+
 </body>
 </html>
