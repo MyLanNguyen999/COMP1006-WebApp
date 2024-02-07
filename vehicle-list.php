11c7b7fa1a0a7f79b66ada55a3f95351ad9290d5
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-02-06 21:11:23 -0500

    Add table of vehicle to vehicle-list page

--- vehicle-list.php
+++ vehicle-list.php
@@ -1,1 +1,40 @@
-``
+<?php
+$title = 'Vehicle List';
+include('shared/header.php');
+
+//* Connect with DB
+$db = new PDO('mysql:host=172.31.22.43;dbname=MyLan200567763', 'MyLan200567763', 'D4DH4hX6-O');
+
+//* Select query to show data from as1_vehicle
+$sql = "SELECT * FROM as1_vehicle";
+$cmd = $db->prepare($sql);
+
+//* Execute the query
+$cmd->execute();
+$as1_vehicles = $cmd->fetchAll();
+
+//* Start the table
+echo '<h1> Vehicle List </h1>';
+
+//* Format table
+echo '<table>
+        <thead>
+            <th> Year </th>
+            <th> Make </th>
+            <th> Model </th>
+            <th> Range </th>
+        </thead>';
+foreach ($as1_vehicles as $as1_vehicle) {
+    echo '<tr>
+            <td> ' . $as1_vehicle['year'] . '</td>
+            <td> ' . $as1_vehicle['make'] . '</td>
+            <td> ' . $as1_vehicle['model'] . '</td>
+            <td> ' . $as1_vehicle['vehicleRange'] . '</td>
+        </tr>';
+}
+
+//* End the table
+echo '</table>';
+//* Disconnect db
+$db = null;
+?>
