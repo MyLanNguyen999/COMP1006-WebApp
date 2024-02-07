91d4de4cdc55304756402d592c4ad861471075d6
MyLanNguyen999 <mylannguyen192@gmail.com>
2024-02-06 21:16:28 -0500

    Add normalize.css

--- shared/header.php
+++ shared/header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
+    <link href="./css/normalize.css" rel="stylesheet" />
+    <link href="./css/styles.css" rel="stylesheet" />
 </head>
 <body>
 <ul>
